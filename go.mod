8c5306a7ba144cddd8e7e448fef45e33617b642c
Ryan Whelan <4249368+rwhelan@users.noreply.github.com>
2022-05-02 16:41:52 -0400

    init: initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rwhelan/chance
+
+go 1.16
---
+++ pkg/chance.go
@@ -0,0 +1,52 @@
+package chance
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Option struct {
+	Name   string
+	Weight int
+}
+
+type Selector struct {
+	r       *rand.Rand
+	options []*Option
+}
+
+func NewSelector() *Selector {
+	return &Selector{
+		r: rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+}
+
+func (s *Selector) AddOption(option ...*Option) {
+	s.options = append(s.options, option...)
+}
+
+func (s *Selector) GetOption() *Option {
+	c := make([]int, len(s.options))
+	c[0] = s.options[0].Weight
+
+	for i := 1; i < len(s.options); i++ {
+		c[i] = c[i-1] + s.options[i].Weight
+	}
+
+	rint := s.r.Intn(c[len(c)-1])
+	for i, v := range c {
+		if rint < v {
+			return s.options[i]
+		}
+	}
+
+	// fmt.Println("THIS SHOULDN'T HAPPEN")
+	return s.options[len(s.options)-1]
+}
+
+func Percent(i int) func() bool {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return func() bool {
+		return r.Intn(100) < i
+	}
+}
